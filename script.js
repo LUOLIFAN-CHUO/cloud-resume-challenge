d07f44872c110063a93e2688fb46275972eb47d6
LUO LIFAN <a72228937@gmail.com>
2026-08-22 11:57:15 +0900

    Add visitor counter fronted using Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("currentDate").textContent =
+    new Date().toLocaleDateString();
+
+const visitorCount = document.getElementById("visitorCount");
+
+visitorCount.textContent = "0";
+
+ console.log("ラ・リキハン｜Cloud Engineer");
