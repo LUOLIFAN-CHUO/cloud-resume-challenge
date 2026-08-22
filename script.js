999000833f6acd6f64b8427da9525a7ce2109ebc
LUO LIFAN <a72228937@gmail.com>
2026-08-22 12:35:15 +0900

    add Api Getway for visitor counter.

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
 document.getElementById("currentDate").textContent =
     new Date().toLocaleDateString();
 
-const visitorCount = document.getElementById("visitorCount");
+const apiUrl = "https://ngknde7bpj.execute-api.ap-northeast-1.amazonaws.com/count";
 
-visitorCount.textContent = "0";
+async function getVisitorCount() {
+    const visitorCountElement = document.getElementById("visitorCount");
+    
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        
+        // 更新页面 DOM 结构
+        visitorCountElement.textContent = data.views;
+    } catch (error) {
+        console.error("Error fetching visitor count:", error);
+        visitorCountElement.textContent = "1"; // 容错备用显示
+    }
+}
+
+getVisitorCount();
 
  console.log("ラ・リキハン｜Cloud Engineer");
