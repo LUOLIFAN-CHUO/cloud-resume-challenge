bf2ab81f3a28f06cc5fbcbdd2cf2a874af3e4fc4
LUO LIFAN <a72228937@gmail.com>
2026-08-24 14:37:07 +0900

    feat: update internship photo and script

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-
-document.getElementById("currentDate").textContent =
-    new Date().toLocaleDateString();
-
 const apiUrl = "https://ngknde7bpj.execute-api.ap-northeast-1.amazonaws.com/count";
 
 async function getVisitorCount() {
     const visitorCountElement = document.getElementById("visitorCount");
-    
+    if (!visitorCountElement) return;
+
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        
-        // 更新页面 DOM 结构
         visitorCountElement.textContent = data.views;
     } catch (error) {
         console.error("Error fetching visitor count:", error);
-        visitorCountElement.textContent = "1"; // 容错备用显示
+        visitorCountElement.textContent = "1";
     }
 }
 
 getVisitorCount();
 
- console.log("ラ・リキハン｜Cloud Engineer");
+console.log("ラ・リキハン｜Cloud Engineer");
